43f68649f2e1e14db2cfc2a1fa4786f999f70982
g13filip <g13filip@gmail.com>
2025-05-21 16:35:59 +0200

    double trouble
    Visitor i listener działają

--- test.py
+++ test.py
@@ -1,34 +1,47 @@
 import streamlit as st
 from antlr4 import *
+from antlr4.CommonTokenStream import CommonTokenStream
+from antlr4.InputStream import InputStream
+
+from Gramatyka.KonturErrorListener import KonturErrorListener
 from Gramatyka.KonturLexer import KonturLexer
 from Gramatyka.KonturParser import KonturParser
 from Gramatyka.interpreter import KonturInterpreter
 
 # Tekstowe pole do wprowadzenia kodu
 st.title("Interpreter języka Kontur")
-code = st.text_area("Wklej kod Kontur:", '''int i = 0;
-while (i < 10) {
-    if (i % 2 == 0) {
-        display(i);
-    }
-    i += 1;
-}''', height=300)
+code = st.text_area("Wprowadź wyrażenie do obliczenia:","", height = 300)
 
 # Przycisk uruchamiający interpreter
 if st.button("Uruchom kod"):
+    if not code.strip():
+        st.warning("Proszę wprowadzić wyrażenie")
     try:
-        # Przetwarzanie kodu źródłowego
+
         input_stream = InputStream(code)
         lexer = KonturLexer(input_stream)
         token_stream = CommonTokenStream(lexer)
         parser = KonturParser(token_stream)
+
+        lexer.removeErrorListeners()
+        parser.removeErrorListeners()
+
+        error_listener = KonturErrorListener()
+        lexer.addErrorListener(error_listener)
+        parser.addErrorListener(error_listener)
+
         tree = parser.program()
 
-        st.subheader("Wynik programu:")
+        if error_listener.errors:
+            st.subheader("Błędy:")
+            for error in error_listener.errors:
+                st.error(error)
+        else:
+            st.subheader("Wynik programu:")
+            interpreter = KonturInterpreter(output_func=st.write)
+            interpreter.visit(tree)
 
-        # Tworzymy interpreter z funkcją wyjściową: st.write
-        interpreter = KonturInterpreter(output_func=st.write)
-        interpreter.visit(tree)
+
 
     except Exception as e:
         st.error(f"Błąd: {e}")
